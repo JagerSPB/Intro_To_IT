ce806b639677378aacad45358d1201d50c265db3
JagerSPB <Jage7@yandex.ru>
2023-03-07 12:49:39 +0300

    Метод принимающий два массива, и заполняет второй с условием меньше 3 знаков

--- HomeWorkIT/Program.cs
+++ HomeWorkIT/Program.cs
@@ -8,6 +8,24 @@ namespace HomeWorkIT
         {
             string[] arrayOrig = new string[7] { "Text1", "AnyText", "if", "false", "res", "var", "49559"};
             string[] arrayNew = new string[arrayOrig.Length];
+
+            void noMoreThan3Array(string[] arr1, string[] arr2)
+            {
+                int count = 0;
+                for (int i = 0; i < arr1.Length; i++)
+                {
+                    if (arr1[i].Length <= 3)
+                    {
+                        arr2[count] = arr1[i];
+                            count++;
+                    }
+                }
+            }
+     
+            
+            noMoreThan3Array(arrayOrig, arrayNew);
+        
+
         }
     }
 }
