cbc9bb6e19c8e024f64cc98b0e38224a0d19d486
JagerSPB <Jage7@yandex.ru>
2023-03-07 12:33:47 +0300

    создал заполненный массив и второй пустой такой-же длины

--- HomeWorkIT/Program.cs
+++ HomeWorkIT/Program.cs
@@ -6,6 +6,8 @@ namespace HomeWorkIT
     {
         public static void Main(string[] args)
         {
+            string[] arrayOrig = new string[7] { "Text1", "AnyText", "if", "false", "res", "var", "49559"};
+            string[] arrayNew = new string[arrayOrig.Length];
         }
     }
 }
